1cde32a2edffa40dbdac5c7f3d8d36be85453345
Brian <analista.ti@crefito4.gov.br>
2023-11-16 14:27:55 -0300

    local storage

--- src/TemplatePetStay-main/js/script.js
+++ src/TemplatePetStay-main/js/script.js
@@ -1,110 +1,90 @@
-const requestDB = window.indexedDB.open("database", 3);
+let usersArray = localStorage.getItem('users') ?
+  JSON.parse(localStorage.getItem('users')) : [];
 
-requestDB.onerror = (event) => console.error('error on db');
+// document.getElementById("cadaster").addEventListener("submit", async (event) => {
+//   event.preventDefault();
 
-requestDB.onsuccess = (event) => {
-    const db = event.target.result;
-    console.log("db created");
+//   let nome = document.getElementById("nome").value;
+//   let email = document.getElementById("email").value;
+//   let phone = document.getElementById("phone").value;
+//   let birth = document.getElementById("birth").value;
+//   let endrc = document.getElementById("endrc").value;
+//   let senha = document.getElementById("password").value;
+//   let confirmaSenha = document.getElementById("confirmPassword").value;
+//   let tipoUser = document.querySelector('input[name="renderOption"]:checked');
+//   let animais = [];
 
-    document.getElementById("cadaster").addEventListener("submit", async (event) => {
-        event.preventDefault();
+//   let ave = document.getElementById("ave");
+//   let cachorro = document.getElementById("cachorro");
+//   let gato = document.getElementById("gato");
 
-        let nome = document.getElementById("nome").value;
-        let email = document.getElementById("email").value;
-        let phone = document.getElementById("phone").value;
-        let birth = document.getElementById("birth").value;
-        let endrc = document.getElementById("endrc").value;
-        let senha = document.getElementById("password").value;
-        let confirmaSenha = document.getElementById("confirmPassword").value;
-        let tipoUser = document.querySelector('input[name="renderOption"]:checked');
-        let animais = [];
+//   if (tipoUser.value == "cuidador") {
+//     ave = document.getElementById("aveCuidador");
+//     cachorro = document.getElementById("cachorroCuidador");
+//     gato = document.getElementById("gatoCuidador");
+//   }
 
-        let ave = document.getElementById("ave");
-        let cachorro = document.getElementById("cachorro");
-        let gato = document.getElementById("gato");
+//   if (ave.checked) {
+//     animais.push("ave");
+//   }
+//   if (cachorro.checked) {
+//     animais.push("cachorro");
+//   }
+//   if (gato.checked) {
+//     animais.push("gato");
+//   }
 
-        if (tipoUser.value == "cuidador") {
-            ave = document.getElementById("aveCuidador");
-            cachorro = document.getElementById("cachorroCuidador");
-            gato = document.getElementById("gatoCuidador");
-        }
+//   let newUser;
+//   if (senha == confirmaSenha) {
+//     newUser = {
+//       tipoUser: tipoUser.value,
+//       nome: nome,
+//       email: email,
+//       phone: phone,
+//       birth: birth,
+//       endrc: endrc,
+//       senha: senha,
+//       tipoAnimal: animais,
+//       nota: 0,
+//       reservas: [],
+//       comments: []
+//     }
 
-        if (ave.checked) {
-            animais.push(ave.value);
-        }
-        if (cachorro.checked) {
-            animais.push(cachorro.value);
-        }
-        if (gato.checked) {
-            animais.push(gato.value);
-}
+//   }
+//   usersArray.push(newUser);
+//   localStorage.setItem("users", JSON.stringify(usersArray));
+// });
 
-        
+let logged = false;
+document.getElementById("login").addEventListener("submit", (event) => {
+  event.preventDefault();
+  email = document.getElementById("emailLogin").value;
+  senha = document.getElementById("senhaLogin").value;
 
-        let newUser;
-        if (senha == confirmaSenha) {
-            if (tipoUser.value == 'donoAnimal') {
-                newUser = {
-                    nome: nome,
-                    email: email,
-                    telefone: phone,
-                    dataNascimento: birth,
-                    endereco: endrc,
-                    senha: senha,
-                    tipoAnimal: animais
-                }
+  usersArray.map((user) => {
+    if(user.email == email){
+      if(user.senha == senha) {
+      logged = true; 
+      console.log(logged);
+      }else{
+        console.log("senha incorreta");
+      }
+      return logged;
+  } else {
+    console.log("Usuario nao encontrado");
+  } 
+  });
+});
 
-                let donoObjStore = db.transaction(["donoAnimal"], "readwrite").objectStore("donoAnimal");
-                let addCuidadorReq = donoObjStore.add(newUser);
-
-                addCuidadorReq.onsuccess = () => console.log("dono do animal adicionado");
-            } else if (tipoUser.value == 'cuidador') {
-                newUser = {
-                    nome: nome,
-                    email: email,
-                    telefone: phone,
-                    dataNascimento: birth,
-                    endereco: endrc,
-                    senha: senha,
-                    tipoAnimalAceito: animais
-                }
-
-                let transaction = db.transaction(["cuidador"], "readwrite");
-                let objStore = transaction.objectStore("cuidador");
-                let addCuidadorReq = objStore.add(newUser);
-                addCuidadorReq.onsuccess = () => console.log("cuidador adicionado");
-            }
-        }
-    });
-};
-
-requestDB.onupgradeneeded = (event) => {
-    const db = event.target.result;
-    db.onerror = (event) => console.error('error on load db');
-
-    if (!db.objectStoreNames.contains('donoAnimal')) {
-        const donoAnimalStore = db.createObjectStore('donoAnimal', { keyPath: "id", autoIncrement: true });
-        donoAnimalStore.createIndex('nomeId', 'nome', { unique: false });
-        donoAnimalStore.createIndex('emailId', 'email', { unique: true });
-        donoAnimalStore.createIndex('telID', 'telefone', { unique: true });
-        donoAnimalStore.createIndex('birthID', 'dataNascimento', { unique: false });
-        donoAnimalStore.createIndex('enderecoID', 'endereco', { unique: true });
-        donoAnimalStore.createIndex('passID', 'senha', { unique: false });
-        donoAnimalStore.createIndex('petsId', 'tipoAnimal', { unique: false, multiEntry: true });
+document.getElementById("resetSenha").addEventListener("submit", (event) => {
+  event.preventDefault();
+  email = document.getElementById("emailResetPass").value;
+  novaSenha = document.getElementById("novaSenha").value;
+  confirma = document.getElementById("confirmaNovaSenha").value;
+  usersArray.map((user) => {
+    if(user.email == email){
+      if(novaSenha == confirma)
+        user.senha = novaSenha;
     }
-
-    if (!db.objectStoreNames.contains('cuidador')) {
-        const cuidadorStore = db.createObjectStore('cuidador', { keyPath: 'id', autoIncrement: true });
-        cuidadorStore.createIndex('nomeId', 'nome', { unique: false });
-        cuidadorStore.createIndex('emailId', 'email', { unique: true });
-        cuidadorStore.createIndex('telID', 'telefone', { unique: true });
-        cuidadorStore.createIndex('birthID', 'dataNascimento', { unique: false });
-        cuidadorStore.createIndex('enderecoID', 'endereco', { unique: true });
-        cuidadorStore.createIndex('passID', 'senha', { unique: false });
-        cuidadorStore.createIndex('acceptedPetsId', 'tipoAnimalAceito', { unique: false, multiEntry: true });
-        cuidadorStore.createIndex('notaId', 'nota', { unique: false });
-        cuidadorStore.createIndex("comments", "comments", { unique: false });
-    }
-};
-
-
+  });
+});
